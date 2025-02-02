e905791ca49bd05f62c6dadcc31fae1de997b095
deepikasurve23 <deepikasurve2304@gmail.com>
2025-02-02 15:50:48 +0530

    part 1 changes done

---
+++ src/main/java/com/Library_management_system/LibraryManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.Library_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
